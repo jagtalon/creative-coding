7873ceb4d804b15669d000a7b71e4af508ab926c
Jag Talon <jag@jagtalon.com>
2020-11-29 04:34:44 -0500

    Run Prettier

--- canvas-noise.js
+++ canvas-noise.js
@@ -1,120 +1,113 @@
-const canvasSketch = require('canvas-sketch');
-const { lerp } = require('canvas-sketch-util/math')
-const random = require('canvas-sketch-util/random')
-const palettes = require('nice-color-palettes')
+const canvasSketch = require("canvas-sketch");
+const { lerp } = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
+const palettes = require("nice-color-palettes");
 
 // This contains the settings for the different drawings.
 const artworks = [
-  // Mountains and valleys
-  {
-    characters: ['▅', '▃', '►', '▔', '▂', '▬', '●'],
-    freq: 1,
-    amp: .7,
-    scale: .04,
-    count: 130,
-    palette: random.pick(palettes),
-    rotation: 4,
-    coordinates: 'grid'
-  },
-  // Squiggly yet flat
-  {
-    characters: ['▅', '▃', '►', '▔', '▂', '▬', '●'],
-    freq: 0,
-    amp: 0,
-    scale: .04,
-    count: 120,
-    palette: random.pick(palettes),
-    rotation: 3,
-    coordinates: 'random'
-  }
-]
+	// Mountains and valleys
+	{
+		characters: ["▅", "▃", "►", "▔", "▂", "▬", "●"],
+		freq: 1,
+		amp: 0.7,
+		scale: 0.04,
+		count: 130,
+		palette: random.pick(palettes),
+		rotation: 4,
+		coordinates: "grid",
+	},
+	// Squiggly yet flat
+	{
+		characters: ["▅", "▃", "►", "▔", "▂", "▬", "●"],
+		freq: 0,
+		amp: 0,
+		scale: 0.04,
+		count: 120,
+		palette: random.pick(palettes),
+		rotation: 3,
+		coordinates: "random",
+	},
+];
 
-random.setSeed(random.getRandomSeed())
-console.log(random.getSeed())
+random.setSeed(random.getRandomSeed());
+console.log(random.getSeed());
 
 const settings = {
-  dimensions: [3600, 5400], // 12x18
-  suffix: "12x18"
+	dimensions: [3600, 5400], // 12x18
+	suffix: "12x18",
 };
 
-
 const sketch = () => {
-  const artwork = artworks[1]
+	const artwork = artworks[1];
 
-  // Fetch the artwork settings.
-  const {
-    palette,
-    count,
-    freq,
-    amp,
-    scale,
-    rotation,
-    characters,
-    coordinates
-  } = artwork
+	// Fetch the artwork settings.
+	const {
+		palette,
+		count,
+		freq,
+		amp,
+		scale,
+		rotation,
+		characters,
+		coordinates,
+	} = artwork;
 
-  // Create the points that we'll be drawing along with their size and rotation.
-  const createGrid = () => {
-    const points = []
+	// Create the points that we'll be drawing along with their size and rotation.
+	const createGrid = () => {
+		const points = [];
 
-    for (let x = 0; x < count; x++) {
-      for (let y = 0; y < count; y++) {
-        let u = count <= 1 ? 0.5 : x / (count - 1)
-        let v = count <= 1 ? 0.5 : y / (count - 1)
+		for (let x = 0; x < count; x++) {
+			for (let y = 0; y < count; y++) {
+				let u = count <= 1 ? 0.5 : x / (count - 1);
+				let v = count <= 1 ? 0.5 : y / (count - 1);
 
-        if (coordinates === 'random') {
-          u = random.value()
-          v = random.value()
-        }
+				if (coordinates === "random") {
+					u = random.value();
+					v = random.value();
+				}
 
-        const radius = random.noise2D(u, v, freq, amp) * .5 + .5
+				const radius = random.noise2D(u, v, freq, amp) * 0.5 + 0.5;
 
-        points.push({
-          fill: random.pick(palette),
-          radius: radius * scale,
-          position: [u, v],
-          rotation: random.noise2D(u, v) * rotation,
-          character: random.pick(characters)
-        })
-      }
-    }
+				points.push({
+					fill: random.pick(palette),
+					radius: radius * scale,
+					position: [u, v],
+					rotation: random.noise2D(u, v) * rotation,
+					character: random.pick(characters),
+				});
+			}
+		}
 
-    return points
-  }
+		return points;
+	};
 
-  const points = createGrid().filter(() => random.value() > .5)
-  const margin = 300
+	const points = createGrid().filter(() => random.value() > 0.5);
+	const margin = 300;
 
-  return ({ context, width, height }) => {
-    // Set the canvas to white because otherwise we'll get a 
-    // transparent background.
-    context.fillStyle = 'white'
-    context.fillRect(0, 0, width, height)
+	return ({ context, width, height }) => {
+		// Set the canvas to white because otherwise we'll get a
+		// transparent background.
+		context.fillStyle = "white";
+		context.fillRect(0, 0, width, height);
 
-    // Draw the points.
-    points.forEach(data => {
-      const {
-        radius,
-        position,
-        fill,
-        rotation,
-        character
-      } = data
-      const [u, v] = position
+		// Draw the points.
+		points.forEach((data) => {
+			const { radius, position, fill, rotation, character } = data;
+			const [u, v] = position;
 
-      const x = lerp(margin, width - margin, u)
-      const y = lerp(margin, height - margin, v)
+			const x = lerp(margin, width - margin, u);
+			const y = lerp(margin, height - margin, v);
 
-      context.save()
-      context.fillStyle = `${fill}E6`
-      context.font = `${radius * width}px "Segoe UI"`
+			context.save();
+			context.fillStyle = `${fill}E6`;
+			context.font = `${radius * width}px "Segoe UI"`;
 
-      context.translate(x, y)
-      context.rotate(rotation)
-      context.fillText(character, 0, 0)
-      context.restore()
-    })
-  };
+			context.translate(x, y);
+			context.rotate(rotation);
+			context.fillText(character, 0, 0);
+			context.restore();
+		});
+	};
 };
 
 canvasSketch(sketch, settings);
--- grid-circle.js
+++ grid-circle.js
@@ -1,76 +1,78 @@
-const canvasSketch = require('canvas-sketch');
-const { lerp } = require('canvas-sketch-util/math')
-const random = require('canvas-sketch-util/random')
+const canvasSketch = require("canvas-sketch");
+const { lerp } = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
-random.setSeed(random.getRandomSeed())
+random.setSeed(random.getRandomSeed());
 
 const settings = {
-    dimensions: [3600, 5400],
-    suffix: "12x18"
+	dimensions: [3600, 5400],
+	suffix: "12x18",
 };
 
 const sketch = () => {
-    // Create a grid of points  
-    const createGrid = () => {
-        const points = []
-        const count = 30
+	// Create a grid of points
+	const createGrid = () => {
+		const points = [];
+		const count = 30;
 
-        for (let x = 0; x < count; x++) {
-            for (let y = 0; y < count; y++) {
-                const u = count <= 1 ? 0.5 : x / (count - 1)
-                const v = count <= 1 ? 0.5 : y / (count - 1)
+		for (let x = 0; x < count; x++) {
+			for (let y = 0; y < count; y++) {
+				const u = count <= 1 ? 0.5 : x / (count - 1);
+				const v = count <= 1 ? 0.5 : y / (count - 1);
 
-                points.push([u, v])
-            }
-        }
+				points.push([u, v]);
+			}
+		}
 
-        return points
-    }
+		return points;
+	};
 
-    // Compute the distance between two points
-    const pointDistance = (circle, point) => {
-        return Math.sqrt(Math.pow(point.x - circle.x, 2) + Math.pow(point.y - circle.y, 2))
-    }
+	// Compute the distance between two points
+	const pointDistance = (circle, point) => {
+		return Math.sqrt(
+			Math.pow(point.x - circle.x, 2) + Math.pow(point.y - circle.y, 2)
+		);
+	};
 
-    const points = createGrid()
-    const margin = 300
+	const points = createGrid();
+	const margin = 300;
 
-    return ({ context, width, height }) => {
-        const circle = {
-            x: width / 2,
-            y: height / 2
-        }
+	return ({ context, width, height }) => {
+		const circle = {
+			x: width / 2,
+			y: height / 2,
+		};
 
-        // Set the canvas to white because otherwise we'll get a 
-        // transparent background.
-        context.fillStyle = 'white'
-        context.fillRect(0, 0, width, height)
+		// Set the canvas to white because otherwise we'll get a
+		// transparent background.
+		context.fillStyle = "white";
+		context.fillRect(0, 0, width, height);
 
-        points.forEach(([u, v]) => {
-            const x = lerp(margin, width - margin, u)
-            const y = lerp(margin, height - margin, v)
+		points.forEach(([u, v]) => {
+			const x = lerp(margin, width - margin, u);
+			const y = lerp(margin, height - margin, v);
 
-            const point = {
-                x: x,
-                y: y
-            }
+			const point = {
+				x: x,
+				y: y,
+			};
 
-            // See if a point is within a circle
-            const distance = pointDistance(circle, point)
-            let circleRadius = width / 2
-            let pointRadius = 25
+			// See if a point is within a circle
+			const distance = pointDistance(circle, point);
+			let circleRadius = width / 2;
+			let pointRadius = 25;
 
-            context.fillStyle = `black`
-            context.beginPath()
+			context.fillStyle = `black`;
+			context.beginPath();
 
-            if (distance <= circleRadius) {
-                pointRadius *= distance > 0 ? distance / circleRadius : .1
-            }
+			if (distance <= circleRadius) {
+				pointRadius *= distance > 0 ? distance / circleRadius : 0.1;
+			}
 
-            context.arc(x, y, pointRadius, 0, 2 * Math.PI, true)
-            context.fill()
-        })
-    };
+			context.arc(x, y, pointRadius, 0, 2 * Math.PI, true);
+			context.fill();
+		});
+	};
 };
 
 canvasSketch(sketch, settings);
